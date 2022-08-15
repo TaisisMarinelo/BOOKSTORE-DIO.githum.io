c81998ab4022811eb5ccaf6d30d0751b0e4dbd7a
TaisisMarinelo <taisismarinelo@gmail.com>
2022-08-15 20:58:32 -0300

    first commit

---
+++ src/app/components/bookstore-app/product-list/product-list.component.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http"
+import { Book } from "./model/book";
+
+
+export const books: Book[] = [
+  { id: '1', name: 'Book1' , price: 10, quantity: 1, categori: "Acao", img:"img1"},
+  { id: '2', name: 'Book2' , price: 10, quantity: 1, categori: "Acao", img:"img2"},
+  { id: '3', name: 'Book3' , price: 10, quantity: 1, categori: "Acao", img:"img3"},
+
+];
+
+@Injectable()
+
+export class BooksService{
+
+    private url = ''; //url API (procurar uma depois)
+    httpOptions={
+      Headers: new HttpHeaders({'content-type': 'application/json'})  
+    } 
+
+    constructor(private http: HttpClient){}
+
+    getBook(){
+        return this.http.get(this.url);
+    }
+
+}
